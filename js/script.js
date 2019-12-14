b229de0d9880b8aee87cdc40359758912cac471e
smithniels <30351969+smithniels@users.noreply.github.com>
2019-12-14 09:51:22 -0500

    1.5

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 console.log("did this work? If you're reading this, yes");
+
+
+$(document).ready(function(){
+  $('.your-class').slick({
+    setting-name: setting-value
+  });
+});
+
+console.log('still working?')
