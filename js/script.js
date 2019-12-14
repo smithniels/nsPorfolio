620876abbb37b16350f4209b7733769f173f5e42
smithniels <30351969+smithniels@users.noreply.github.com>
2019-12-14 10:07:23 -0500

    1.6 or something

--- js/script.js
+++ js/script.js
@@ -1,10 +1,5 @@
 console.log("did this work? If you're reading this, yes");
 
-
-$(document).ready(function(){
-  $('.your-class').slick({
-    setting-name: setting-value
-  });
+$(document).ready(function() {
+  console.log("still working?");
 });
-
-console.log('still working?')
