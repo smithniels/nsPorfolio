8f6f8107d7b9dc8209fe2d7d96cdab61f14916b2
smithniels <30351969+smithniels@users.noreply.github.com>
2019-12-14 10:27:49 -0500

    1.7

--- js/script.js
+++ js/script.js
@@ -2,4 +2,35 @@ console.log("did this work? If you're reading this, yes");
 
 $(document).ready(function() {
   console.log("still working?");
+
+  $("#slick-images").slick({
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 });
