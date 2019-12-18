9a79b5473c72e9a16f017685fecaf8b17cc6e0cb
Niels Smith <30351969+smithniels@users.noreply.github.com>
2019-12-18 14:43:14 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -19,15 +19,15 @@ $(document).ready(function() {
   $("#header").headroom();
 });
 
+// Siema means 'hello' in Polish
 var slideIndex = 0;
 var numSlides = 4;
 
 const slideshow = new Siema({
   selector: ".slider",
   loop: true,
-  // startIndex: 1,
-  // draggable: false,
-  threshold: 200
+  threshold: 200,
+  easing: "cubic-bezier(.17,.67,.32,1.34)"
 });
 
 $(".prev").on("click", function() {
