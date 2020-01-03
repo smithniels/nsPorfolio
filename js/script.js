1b76640034884a9848ad8c7587c676492f91d27f
smithniels <30351969+smithniels@users.noreply.github.com>
2020-01-03 08:03:55 -0500

    18.1

--- js/script.js
+++ js/script.js
@@ -9,9 +9,8 @@ $(document).ready(function() {
     navText: [
       '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
       '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>'
+      // VIEWBOX^ IS POSITION WITHIN THE BOX
     ],
-// VIEWBOX^ IS POSITION WITHIN THE BOX
-
     pagination: false,
     dots: false,
     autoplay: true,
