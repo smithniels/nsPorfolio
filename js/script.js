3e35f6f8b173931e4e69d6e4c860cafea179fa8a
smithniels <smithniels@gmail.com>
2020-01-20 09:35:42 -0500

    35

--- js/script.js
+++ js/script.js
@@ -10,45 +10,45 @@ $.each( $('*'), function() {
   else{console.log("no extra widths!")}
 });
 
-  //OWL
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    navText: [
-      '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
-      '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>'
-      // VIEWBOX^ IS POSITION WITHIN THE BOX
-    ],
-    pagination: false, 
-    dots: true,
-    lazyLoad: true,
-    autoplay: true,
-    autoplayHoverPause: true,
-    autoplaySpeed: 2000,
-    slideSpeed: 3000,
-    paginationSpeed: 9000,
-    stagePadding: 70,
-    responsiveClass:true,
-    responsive: {
-      0: {
-        // stagePadding: 0,
-        items: 1,
-        dots: false,
-      },
-      600: {
-        stagePadding: 0,
-        items: 1
-      },
-      1000: {
-        items: 1
-      }
+//OWL
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  navText: [
+    '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z"/></svg>',
+    '<svg width="50" height="50" viewBox="0 0 24 24"><path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z"/></svg>'
+    // VIEWBOX^ IS POSITION WITHIN THE BOX
+  ],
+  pagination: false, 
+  dots: true,
+  lazyLoad: true,
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplaySpeed: 2000,
+  slideSpeed: 3000,
+  paginationSpeed: 9000,
+  stagePadding: 70,
+  responsiveClass:true,
+  responsive: {
+    0: {
+      // stagePadding: 0,
+      items: 1,
+      dots: false,
+    },
+    600: {
+      stagePadding: 0,
+      items: 1
+    },
+    1000: {
+      items: 1
     }
-  });
-  $('.owl-carousel').owlCarousel({
-    items:1,
-    margin:10,
-    // autoHeight:true
+  }
+});
+$('.owl-carousel').owlCarousel({
+  items:1,
+  margin:10,
+  // autoHeight:true
 });
 
 // BROKEN SCROLL THING
