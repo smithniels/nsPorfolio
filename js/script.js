414469454ed3b9ab7f5343208e2d9d5ef9cda26e
smithniels <smithniels@gmail.com>
2020-02-02 16:16:51 -0500

    wow css much?

--- js/script.js
+++ js/script.js
@@ -148,39 +148,6 @@ $(document).ready(function() {
       console.log('To The Top!')
       return false;
   });
-  // Fade In/Out
-    function fadeInPage() {
-        if (!window.AnimationEvent) { return; }
-        var fader = document.getElementById('fader');
-        fader.classList.add('fade-out');
-    }
-
-    document.addEventListener('DOMContentLoaded', function() {
-        if (!window.AnimationEvent) { return; }
-        
-        var anchors = document.getElementsByTagName('a');
-        
-        for (var idx=0; idx<anchors.length; idx+=1) {
-            if (anchors[idx].hostname !== window.location.hostname ||
-                anchors[idx].pathname === window.location.pathname) {
-                continue;
-            }
-
-            anchors[idx].addEventListener('click', function(event) {
-                var fader = document.getElementById('fader'),
-                anchor = event.currentTarget;
-                
-                var listener = function() {
-                    window.location = anchor.href;
-                    fader.removeEventListener('animationend', listener);
-                };
-                fader.addEventListener('animationend', listener);
-                
-                event.preventDefault();
-                
-                fader.classList.add('fade-in');
-            });
-        }
-    });
+  
 
 });
