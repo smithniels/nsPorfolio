eea70595111efe81a46f9ef373ce32943d37010f
smithniels <smithniels@gmail.com>
2020-02-22 18:56:03 -0500

    oh I should make it work before I push it
    mb

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ function fadeInPage() {
   var fader = document.getElementById('fader');
   fader.classList.add('fade-out');
 }
+
 const hero = document.querySelector('.hero');
 const text = hero.querySelector('h1');
 const walk = 500; // 500px
