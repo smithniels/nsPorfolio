a5949f21f438cc0a7dc6f191b83a6cd7437d51e0
Niels Smith <30351969+smithniels@users.noreply.github.com>
2020-07-01 12:45:25 -0400

    .off( "focus" )
    .off( "focus" )

--- js/script.js
+++ js/script.js
@@ -190,6 +190,7 @@ $('a[href*="#"]')
         } else {
           $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
           $target.focus(); // Set focus again
+          $target.off( "focus" )
         };
       });
     }
