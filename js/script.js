0ba63f5d268d3a85f83cbf3192dd29d21b0b7386
Niels Smith <30351969+smithniels@users.noreply.github.com>
2020-07-02 07:24:16 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -194,8 +194,8 @@ $('a[href*="#"]')
             } else {
               $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
               $target.focus(); // Set focus again
-              $target.off(":focus"); // Niels' addition in attempt to get that focus outline outta here
-              // $target.blur(); // removes focus! //This bit works, but I want to see what .off("focus") does
+              // $target.off(":focus"); // Niels' addition in attempt to get that focus outline outta here // this didn't work
+              $target.blur(); // removes focus! //This bit works, but I want to see what .off("focus") does (.off didn't work)
             }
           }
         );
