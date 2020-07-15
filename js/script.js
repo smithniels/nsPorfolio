f333af441d7a7f32ff867c63de0f626b0540bce3
Niels Smith <30351969+smithniels@users.noreply.github.com>
2020-07-15 12:43:11 -0400

    remove header hider

--- js/script.js
+++ js/script.js
@@ -87,48 +87,48 @@ $(".owl-carousel").owlCarousel({
   autoHeight: true,
 });
 
-// scrolly header bits
-// scrolly header bits
-// scrolly header bits
-// Credit to Marius Craciunoiu
+// // scrolly header bits
+// // scrolly header bits
+// // scrolly header bits
+// // Credit to Marius Craciunoiu
 
-// Hide Header on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $("header").outerHeight();
+// // Hide Header on on scroll down
+// var didScroll;
+// var lastScrollTop = 0;
+// var delta = 5;
+// var navbarHeight = $("header").outerHeight();
 
-$(window).scroll(function (event) {
-  didScroll = true;
-});
+// $(window).scroll(function (event) {
+//   didScroll = true;
+// });
 
-setInterval(function () {
-  if (didScroll) {
-    hasScrolled();
-    didScroll = false;
-  }
-}, 250);
+// setInterval(function () {
+//   if (didScroll) {
+//     hasScrolled();
+//     didScroll = false;
+//   }
+// }, 250);
 
-function hasScrolled() {
-  var st = $(this).scrollTop();
+// function hasScrolled() {
+//   var st = $(this).scrollTop();
 
-  // Make sure they scroll more than delta
-  if (Math.abs(lastScrollTop - st) <= delta) return;
+//   // Make sure they scroll more than delta
+//   if (Math.abs(lastScrollTop - st) <= delta) return;
 
-  // If they scrolled down and are past the navbar, add class .nav-up.
-  // This is necessary so you never see what is "behind" the navbar.
-  if (st > lastScrollTop && st > navbarHeight) {
-    // Scroll Down
-    $("header").removeClass("nav-down").addClass("nav-up");
-  } else {
-    // Scroll Up
-    if (st + $(window).height() < $(document).height()) {
-      $("header").removeClass("nav-up").addClass("nav-down");
-    }
-  }
+//   // If they scrolled down and are past the navbar, add class .nav-up.
+//   // This is necessary so you never see what is "behind" the navbar.
+//   if (st > lastScrollTop && st > navbarHeight) {
+//     // Scroll Down
+//     $("header").removeClass("nav-down").addClass("nav-up");
+//   } else {
+//     // Scroll Up
+//     if (st + $(window).height() < $(document).height()) {
+//       $("header").removeClass("nav-up").addClass("nav-down");
+//     }
+//   }
 
-  lastScrollTop = st;
-}
+//   lastScrollTop = st;
+// }X
 
 // scroll-ee bits => bottom to top
 // scroll-ee bits => bottom to top
@@ -196,7 +196,7 @@ $('a[href*="#"]')
               $target.focus(); // Set focus again
               // $target.off(":focus"); // Niels' addition in attempt to get that focus outline outta here // this didn't work
               $target.blur(); // removes focus! //This bit works, but I want to see what .off("focus") does (.off didn't work)
-              console.log("this is updated now")
+              console.log("this is updated now");
             }
           }
         );
