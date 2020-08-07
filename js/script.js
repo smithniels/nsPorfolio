329c739a393d71464da4013245372f2f370ce187
Niels Smith <30351969+smithniels@users.noreply.github.com>
2020-08-07 09:05:05 -0400

    how does JS work?

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+function changeWords() {
+  var words = document.getElementById("words");
+  words.classList.add("test");
+  console.log("words are hovered");
+}
+
 // Fade Into/Out of page
 // Fade Into/Out of page
 // Fade Into/Out of page
@@ -87,53 +93,6 @@ $(".owl-carousel").owlCarousel({
   autoHeight: true,
 });
 
-// // scrolly header bits
-// // scrolly header bits
-// // scrolly header bits
-// // Credit to Marius Craciunoiu
-
-// // Hide Header on on scroll down
-// var didScroll;
-// var lastScrollTop = 0;
-// var delta = 5;
-// var navbarHeight = $("header").outerHeight();
-
-// $(window).scroll(function (event) {
-//   didScroll = true;
-// });
-
-// setInterval(function () {
-//   if (didScroll) {
-//     hasScrolled();
-//     didScroll = false;
-//   }
-// }, 250);
-
-// function hasScrolled() {
-//   var st = $(this).scrollTop();
-
-//   // Make sure they scroll more than delta
-//   if (Math.abs(lastScrollTop - st) <= delta) return;
-
-//   // If they scrolled down and are past the navbar, add class .nav-up.
-//   // This is necessary so you never see what is "behind" the navbar.
-//   if (st > lastScrollTop && st > navbarHeight) {
-//     // Scroll Down
-//     $("header").removeClass("nav-down").addClass("nav-up");
-//   } else {
-//     // Scroll Up
-//     if (st + $(window).height() < $(document).height()) {
-//       $("header").removeClass("nav-up").addClass("nav-down");
-//     }
-//   }
-
-//   lastScrollTop = st;
-// }X
-
-// scroll-ee bits => bottom to top
-// scroll-ee bits => bottom to top
-// scroll-ee bits => bottom to top
-
 $(window).scroll(function () {
   // Is the window more than 100px away?
   if ($(this).scrollTop() > 100) {
@@ -203,17 +162,3 @@ $('a[href*="#"]')
       }
     }
   });
-
-function changeWords() {
-  var words = document.getElementById("words");
-  words.classList.add("test");
-  console.log("words are hovered");
-}
-
-// function fadeInPage() {
-//   if (!window.AnimationEvent) {
-//     return;
-//   }
-//   var fader = document.getElementById("fader");
-//   fader.classList.add("fade-out");
-// }
