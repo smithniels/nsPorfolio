ff5c0ec60ec9cae869e6c2dec8787a72c719808c
Niels Smith <30351969+smithniels@users.noreply.github.com>
2021-03-29 09:41:53 -0400

    starting color selector proj

---
+++ js/color.js
@@ -0,0 +1,8 @@
+const colors = ["#69dfd2", "red", "green"];
+const btn = document.getElementByID("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNumber = 2;
+  document.body.style.backgroundColor = colors[randomNumber];
+});
